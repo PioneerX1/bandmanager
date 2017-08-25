82df8322ae0f7cfeb6b787d397c85d2667289dbe
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-25 11:57:38 -0700

    add member service to deliver list of band members from firebase on homepage

--- src/app/mock-members.ts
+++ src/app/mock-members.ts
@@ -1,10 +1,10 @@
-import { Member } from './member.model';
-
-export const MEMBERS: Member[] = [
-  new Member("Guiseppe the Face", "301-343-8848", "theface@facemail.com", "trumpet", "arranging", true),
-  new Member("Sam Smooth", "206-323-1241", "samsmooth@gmail.com", "alto saxophone", "lead and soloing", true),
-  new Member("Johnny Good", "206-393-4442", "jbgood@hotmail.com", "guitar", "original composition", true),
-  new Member("Big Walter", "404-222-7676", "walterdoesthewaltz@whatmail.com", "double bass", "marketing", false),
-  new Member("Leroy Jenkins", "303-454-1111", "leroysocoy@justaboy.com", "drums", "passing the tip jar", true),
-  new Member("Larry the Lion", "872-222-8753", "larrylion@hotmail.com", "trombone", "", false)
-];
+// import { Member } from './member.model';
+//
+// export const MEMBERS: Member[] = [
+//   new Member("Guiseppe the Face", "301-343-8848", "theface@facemail.com", "trumpet", "arranging", true),
+//   new Member("Sam Smooth", "206-323-1241", "samsmooth@gmail.com", "alto saxophone", "lead and soloing", true),
+//   new Member("Johnny Good", "206-393-4442", "jbgood@hotmail.com", "guitar", "original composition", true),
+//   new Member("Big Walter", "404-222-7676", "walterdoesthewaltz@whatmail.com", "double bass", "marketing", false),
+//   new Member("Leroy Jenkins", "303-454-1111", "leroysocoy@justaboy.com", "drums", "passing the tip jar", true),
+//   new Member("Larry the Lion", "872-222-8753", "larrylion@hotmail.com", "trombone", "", false)
+// ];
